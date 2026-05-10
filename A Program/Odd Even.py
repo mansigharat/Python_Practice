3a1f5f66f3e68f7b2966957417bc6d9723b366e2
Mansi Gharat <mansigharat1909@gmail.com>
2026-05-10 19:07:00 +0530

    All program is very good, just read some comment again

--- A Program/Odd Even.py
+++ A Program/Odd Even.py
@@ -18,3 +18,13 @@ else:
 #     print(a,"is an Even Number")    
 # elif (b==1):
 #     print(a, "is an Odd Number")
+
+# i have another approach
+
+num = int(input("Enter a number : "))
+if(num%2==0):
+    print("Number is even")
+else:
+    print("Number is odd")
+
+# for multiline comment just select it code and use ctrl + /
--- A Program/Temperature Converter.py
+++ A Program/Temperature Converter.py
@@ -2,4 +2,6 @@ int = float(input("Enter the Temperature in Celsius: "))
 
 Fahrenheit = (int*(9/5))+32
 
-print('The Conversion of the Temperature in Fahrenheit is:',Fahrenheit)
+print(f"The Conversion of the Temperature in Fahrenheit is: {Fahrenheit:.2f}")
+
+# .2f just give 2 digit after decimal number
