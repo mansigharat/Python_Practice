c801269623689d7b0e79ab4d55ba1b41353a21d5
atharvathorve2@gmail.com <Atharvathorve2@gmail.com>
2026-05-20 23:26:41 +0530

    <3

---
+++ A Program/contact.py
@@ -0,0 +1,8 @@
+contact = {
+    'name': 'Atharva',
+    'phone': 19090711,
+    'city': 'Pen',
+}
+print(contact['name'])
+print(contact['phone']) 
+print(contact['city'])
---
+++ A Program/test.py
@@ -0,0 +1,7 @@
+info = {'a': 1, 'b': 2, 'c': 3} 
+for key in info: 
+    print(key) 
+for value in info.values(): 
+    print(value) 
+for key, value in info.items():    
+    print(f'{key} -> {value}')
