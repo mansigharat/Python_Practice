4d8fa504d53d847cb47df67c4fca2420ec1da843
Mansi Gharat <mansigharat1909@gmail.com>
2026-06-03 16:28:10 +0530

    python files

---
+++ M Program/4_Lists/1_index.py
@@ -0,0 +1,7 @@
+a = [12,45,67,43,89,32] #first way
+
+for i in range(len(a)):
+    print(a[i])
+
+for i in a: #second way
+    print(i)
---
+++ M Program/4_Lists/2_methods.py
@@ -0,0 +1,11 @@
+a = [1,2,3,4,5,6,7]
+
+# a.append(8)
+# print(a)
+
+# a.insert(7,8)
+# print(a)
+
+# a.remove(2)
+# print(a)
+
