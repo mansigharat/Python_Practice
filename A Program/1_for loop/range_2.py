2780b26b8f88e92284161597453182b0e5ca38ce
atharvathorve2@gmail.com <Atharvathorve2@gmail.com>
2026-06-08 13:11:19 +0530

    fine :'(

---
+++ A Program/1_for loop/range_2.py
@@ -0,0 +1,4 @@
+n=int(input("Which number table do you want ? :"))
+
+for i in range(n,(n*10)+1,n):
+    print(i)  
---
+++ A Program/1_for loop/range_3.py
@@ -0,0 +1,8 @@
+# a = "ATHARVA "    
+# print (len(a))
+# for i in range(len(a)):
+#     print(a[i])
+
+a = "ATHARAVA WILL BECOME AN EXCELLENT AIML ENGINEER"
+for i in a:
+    print(i)
