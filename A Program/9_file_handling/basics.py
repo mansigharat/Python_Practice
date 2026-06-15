340da53df2f8b4d97433399626872c9de1e25c98
mansigharat <mansigharat1909@gmail.com>
2026-06-15 23:55:22 +0530

    new lot of work

---
+++ A Program/9_file_handling/basics.py
@@ -0,0 +1,16 @@
+# reading a file
+p = open('C:\\Users\\Dell\\Python_Practice\\A Program\\8_exception_handing\\raise.py')
+
+print(p.read())
+
+#creating and appending to a file
+
+r = open('demo_for_append.txt', 'w') # 'w' is used to create a new file, if the file already exists it will overwrite the existing file
+
+r.write('This is a demo file for append') #whatever is written in this parenthesis will be written in the main file
+
+r = open('demo_for_append.txt', 'a') # 'a' is used to append to a file, if the file does not exist it will create a new file
+ 
+r.write('\nI can add as many lines as I want using the append (a)') #whatever is written in this parenthesis will be written in the main file
+
+r.close()
