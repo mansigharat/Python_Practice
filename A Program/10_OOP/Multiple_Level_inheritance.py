95036e72735b33c39da7c0fa7e23e2ed764d8ed2
mansigharat <atharvathorve2@gmail.com>
2026-06-18 14:27:48 +0530

    Be Consistant

--- A Program/10_OOP/Multiple_Level_inheritance.py
+++ A Program/10_OOP/Multiple_Level_inheritance.py
@@ -19,22 +19,3 @@ print(obj.age)
 
 bot = Robots("Charlie123",19,"m9")
 print(bot.name, bot.age, bot.model)
-
-
-# multilevel inheritance
-class haryanafactory:
-    def __init__(self,wood,grip):
-        self.wood = wood
-        self.grip = grip
-
-class delhifactory(haryanafactory):
-    def __init__(self, wood, grip,sticker):
-        super().__init__(wood, grip)
-        self.sticker = sticker
-
-class mumbaifactory(delhifactory):
-    def __init__(self, wood, grip, sticker, costing):
-        super().__init__(wood, grip, sticker)
-        self.costing = costing
-
-
--- A Program/10_OOP/Polymorphism.py
+++ A Program/10_OOP/Polymorphism.py
@@ -10,3 +10,19 @@ class Human(Animal):
 
 obj = Human()
 obj.show()
+
+#duck typing
+
+class Animal:
+    def show(self):
+        print("Vanilla likes Chocolate.")
+
+class Human:
+    def show(self):
+        print("Chocolate likes Vanilla.")
+
+obj = Human()
+obj2 = Animal()
+
+obj.show()
+obj2.show()
