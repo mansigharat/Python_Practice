c9f0475bc849835e0cafd2890ac1148d8378948c
Mansi Gharat <mansigharat1909@gmail.com>
2026-06-19 15:03:34 +0530

    module and packages

--- M Program/11_Advanced_Python/3_Lambda_Function.py
+++ M Program/11_Advanced_Python/3_Lambda_Function.py
@@ -1,2 +1,12 @@
-addition = lambda a,b : a+b 
-print(addition(2,5))
+addition = lambda a : "even" if a%2 == 0 else "odd"
+print(addition(5))
+
+cube = lambda a : a*a*a 
+print(cube(3))
+
+string = input("enter the string : ")
+upper = lambda string : string.upper()
+print(upper(string))
+
+large = lambda a,b : f"{a} is Large" if a>b  else f"{b} is large"
+print(large(5,7))
--- M Program/4_Lists/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-a = [1,2,3,4,5,6]
-for i in range(len(a)):
-    if a[i] < a[i+1]:
-        continue
-    else:
-        print("Your list is not sorted")
-        break
-    
