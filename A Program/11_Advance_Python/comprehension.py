9929121a2d357f65bc7cd5ec8e0301ace95aa53c
AtharvaThorve <atharvathorve2@gmail.com>
2026-06-20 22:24:30 +0530

    wee

---
+++ A Program/11_Advance_Python/comprehension.py
@@ -0,0 +1,21 @@
+squares = [x**2 for x in range(10)]
+print(squares)
+
+l = [i for i in range(1,21) if i%2==0] 
+print(l)
+
+s = {x*x for x in range(10) if x%2 ==0}
+print(s)
+
+d = {i : i**2 for i in range(1,11)}
+print(d)
+
+l = [i*i for i in range(1,21) if i%2 !=0]
+print(l)
+
+l = ["agent", "model", "task", "memory", "tool"]
+d = {i:len(i) for i in l }
+print(d)
+
+s = {x for x in range(1,30) if x%3 ==0 or x%5==0} 
+print(s)
