e41127c5bd8d4dd3ee82c539a773c1fcf2e9fe59
AtharvaThorve <atharvathorve2@gmail.com>
2026-06-21 23:38:57 +0530

    g

---
+++ A Program/12_type_hint/1_type_hint.py
@@ -0,0 +1,39 @@
+# normally
+def add(a, m):
+    return a + m
+
+
+# using type hints
+def add(a: int, m: int) -> int:
+    return a + m
+
+
+def info(name: str, age: int) -> str:
+    return f"My name is {name} and I am {age} year old."
+print(info("Mansi",19))
+
+
+def sub(a: int, m: int) -> int:
+    return a - m
+print(sub(19,7))
+
+
+def mul(x: int, y: int) -> int:
+    return x*y
+print(mul(19,7))
+
+
+def add_return(a,m):
+    return a+m
+
+def add_print(a,m):
+    print(a+m)
+
+
+# return version
+result = add_return(2,3)
+print(result * 2)
+
+
+# print version
+result = add_print(2,3)
