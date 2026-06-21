4f49bdd811190353a6b9af2c116297be562ad8e3
AtharvaThorve <atharvathorve2@gmail.com>
2026-06-21 22:14:50 +0530

    chocolate

---
+++ A Program/11_Advance_Python/3_lambda_func.py
@@ -0,0 +1,24 @@
+addition = lambda a, b : a + b
+
+print(addition(19,7))
+
+square = lambda m : m*m
+
+print(square(9))
+
+cube = lambda m : m*m*m 
+
+print(cube(5))
+
+number = lambda a : "EVEN" if a%2 == 0 else "ODD"
+
+print(number(19))
+
+age = lambda a,m : f"{a} is older than {m}" if a>m  else f"{m} is older than {a}"
+
+print(age(19,20))
+
+Sentence = input("Chocolate and Vanilla are the best combination.")
+line = lambda string : string.line()
+
+print(line(Sentence))
