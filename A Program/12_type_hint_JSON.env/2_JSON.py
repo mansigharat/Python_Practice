e57e0a59fa105dbf6a076f43287703ebed79fb47
AtharvaThorve <atharvathorve2@gmail.com>
2026-06-23 22:52:03 +0530

    JSON

---
+++ A Program/12_type_hint_JSON.env/2_JSON.py
@@ -0,0 +1,61 @@
+# import json
+
+# person = {
+#     "name": "Chocolate",
+#     "age": 21,
+#     "skills": ["Python", "AI"]
+# }
+
+# # dict to string
+# json_string = json.dumps(person)
+# print(type(json_string))
+# print(json_string)
+
+# # string back to dict
+# back_to_dict = json.loads(json_string)
+# print(type(back_to_dict))
+# print(back_to_dict["name"])
+
+
+
+# job = {
+#     "name": "Atharva Thorve",
+#     "age": "20",
+#     "mob number" : "9108910899",
+
+# }
+
+# import json
+
+# person = {"name": "Chocolate", "age": 21}
+
+# json_string = json.dumps(person)
+
+# print(json_string["name"])  # this will crash. why?
+
+
+
+
+
+# 3rd code
+
+import json
+from pathlib import Path
+
+data = {
+    "agent" : "sales_bot",
+    "last _action" : "sent_email",
+    "success" : True
+}
+
+file = Path ("agent_log.json")
+
+with file.open("w") as f:
+    json.dump(data, f, indent=2)
+
+with file.open("r") as f:
+    loaded = json.load(f)
+
+
+print(loaded["agent"])
+print(loaded["success"])
