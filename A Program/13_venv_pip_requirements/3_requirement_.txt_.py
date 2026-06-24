9cede543fad9dfa7e63e993b0a22b373c6d8c164
AtharvaThorve <atharvathorve2@gmail.com>
2026-06-24 22:53:48 +0530

    PIP

---
+++ A Program/13_venv_pip_requirements/3_requirement_.txt_.py
@@ -0,0 +1,14 @@
+# Requirement is a text file which stores the required packages with their versions.
+
+# mkdir weather-app
+# cd weather-app
+
+# python -m venv venv
+
+# venv\Scripts\activate
+
+# pip install httpx 
+
+# pip freeze > requirments.txt
+
+# deactivate
