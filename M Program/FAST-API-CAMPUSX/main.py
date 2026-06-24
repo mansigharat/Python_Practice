e75ba1c4e3c3b016c3065dd846f61b237c272183
Mansi Gharat <mansigharat1909@gmail.com>
2026-06-24 14:59:08 +0530

    Path Param and Query Param

--- M Program/FAST-API-CAMPUSX/main.py
+++ M Program/FAST-API-CAMPUSX/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path,HTTPException
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 
 app = FastAPI()
@@ -21,6 +21,9 @@ def view():
     data = load_data()
     return data
 
+
+#Path Parameter
+
 @app.get('/patient/{patient_id}')
 def view_patient(patient_id: str = Path(..., description= 'ID of the patient in the DB',example = 'P001')):
     #load all the patients
@@ -30,3 +33,27 @@ def view_patient(patient_id: str = Path(..., description= 'ID of the patient in 
         return data[patient_id]
     raise HTTPException(status_code = 404, detail = "Patient Not Found")
 
+
+#Query Parameter
+@app.get('/sort')
+def sort_patients(
+    sort_by:str = Query(..., 
+                        description = "Sort on the basis of height weight and BMI") , 
+                        order : str = Query('asc',description = 'Sort by asecending and descending order'
+                        )
+    ):
+    valid_fields = ['height' , 'weight' ,'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code = 400,detail = f'Invalid filed select from{valid_fields}')
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code = 400 , detail = 'Invalid order select between asc and desc')
+    
+    data = load_data()
+
+    sort_order = True if order == 'desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x:x.get(sort_by ,0), reverse= sort_order)
+
+    return sorted_data
