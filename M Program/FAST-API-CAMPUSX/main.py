a017e7a6713e1fbfeca01fe1f1a6e5fce9650ac1
Mansi Gharat <mansigharat1909@gmail.com>
2026-06-29 17:04:40 +0530

    PUT

--- M Program/FAST-API-CAMPUSX/main.py
+++ M Program/FAST-API-CAMPUSX/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
 import json
 from pydantic import BaseModel,Field, computed_field
-from typing import Annotated,Literal
+from typing import Annotated,Literal, Optional
 
 app = FastAPI()
 
@@ -34,6 +34,16 @@ class Patient(BaseModel):
             return 'Normal'
         else:
             return 'Obese'
+
+class PatientUpdate(BaseModel):
+
+    name : Annotated[Optional[str],Field(default = None)]
+    city: Annotated[Optional[str],Field(default = None)]
+    age : Annotated[Optional[int],Field(default = None,gt = 0)]
+    gender : Annotated[Optional[Literal ['male','female','others']],Field(default = None)]
+    height : Annotated[Optional[float],Field(default = None,gt = 0)]
+    weight : Annotated[Optional[float],Field(default = None,gt = 0)]
+
 
 def load_data():
     with open("patients.json", "r") as f:
@@ -137,3 +147,33 @@ def create_patient(patient:Patient):
     save_data(data)
 
     return JSONResponse(status_code = 201, content = {'message':'Patient Created Successfully'})
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str , patient_update: PatientUpdate):
+    data = load_data()
+    if patient_id not in data:
+        raise HTTPException(status_code= 404 , detail = 'Patient not found')
+    
+    exisiting_patient_info = data[patient_id]
+
+    updated_patient_info = patient_update.model_dump(exclude_unset = True)
+
+    for key , value in updated_patient_info.items():
+        exisiting_patient_info[key] = value 
+
+    exisiting_patient_info['id'] = patient_id
+    Patient_pydantic_obj = Patient(**exisiting_patient_info)
+    exisiting_patient_info = Patient_pydantic_obj.model_dump(exclude = 'id')
+    #  exisiting_patient_info  
+
+
+    data[patient_id] = exisiting_patient_info 
+
+    save_data(data)
+
+    return JSONResponse(status_code = 200 ,content={'message':'patient updated'})
+
+# PUT = updating
+# GET = retrieve
+# POST = Creating
+# DELETE = deleation
