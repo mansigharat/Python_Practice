2e2f199f04660178bc8b89cfaa89ccffd6fc3e47
Atharva9988 <atharvathorve2@gmail.com>
2026-07-07 14:52:20 +0530

    07/07/2026

--- A Program/15_pydantic/Trial.py
+++ A Program/15_pydantic/Trial.py
@@ -1,1 +1,39 @@
-print ("Hello")
+# # code 1 
+
+# class House:
+#     def __init__(self, color, rooms):
+#         self.color = color
+#         self.rooms = rooms
+
+#     def describe(self):
+#         print(f"This house is {self.color} and has {self.rooms} rooms.")
+
+
+# house1 = House("Blue", 3)
+# house2 = House("White", 5)
+
+# print(house1.color)
+# print(house2.rooms)
+
+# house1.describe()
+# house2.describe()
+
+
+
+# # code 2
+
+# class House:
+#     def __init__(self, color: str, rooms: int):
+#         self.color: str = color
+#         self.rooms: int = rooms
+
+#     def describe(self) -> None:
+#         print(f"This house is {self.color} and has {self.rooms} rooms.")
+
+
+# house1 = House("Blue", 3)
+# house2 = House("White", 5)
+
+# house1.describe()
+# house2.describe()
+
