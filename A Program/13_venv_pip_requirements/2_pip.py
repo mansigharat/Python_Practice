cd6fb68b57448efcc960cb31138c8b55e7180294
Atharva9988 <atharvathorve2@gmail.com>
2026-07-12 16:20:13 +0530

    12/07/2026

--- A Program/13_venv_pip_requirements/2_pip.py
+++
@@ -1,4 +0,0 @@
-# PIP is pythons package manager, it helps to download update delete and manage libraries. 
-# PIP is like a delivery boy who just simply brings different items for us, or else we have to do so.
-# In technical words it downloads libraries on one simple command.
-# Or else we manually have to download those libraries.
